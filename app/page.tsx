b17e8886df988d1c0b120897d6f038fc569fc0cb
Timothy Fawcett <tim.fawcett89@gmail.com>
2023-06-11 22:24:00 +1000

    fixed name problem and made some types optional to fix error

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,11 @@ interface Country {
   };
   name: {
     common: string;
+    official: string;
   };
-  population: number;
-  capital: string;
-  region: string;
+  population?: number;
+  capital?: string;
+  region?: string;
 }
 
 // Define regions for the dropdown filter
@@ -104,7 +105,7 @@ export default function Page() {
   }, []);
 
   // Render loading state
-  if (isLoading) return <p>Loading...</p>;
+  if (isLoading) return <svg className="h-10 w-10 mx-auto my-10 animate-spin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM208.6 357.3l-39-13.5c-6.5-2.2-13.6-2.3-20.1-.3l-15.3 4.9c-18.5 5.9-38.5-2.4-47.5-19.5l-3.3-6.2c-10.6-20.1-2.3-45 18.2-54.7l35.3-16.8c2.3-1.1 4.4-2.8 5.9-4.8l5.3-7c7.2-9.6 18.6-15.3 30.6-15.3s23.4 5.7 30.6 15.3l4.6 6.1c2 2.6 4.9 4.5 8.1 5.1c7.8 1.6 15.7-1.5 20.4-7.9l10.4-14.2c2-2.8 5.3-4.4 8.7-4.4c4.4 0 8.4 2.7 10 6.8l10.1 25.9c2.8 7.2 6.7 14 11.5 20.2L311 299.8c5.8 7.4 9 16.6 9 26s-3.2 18.6-9 26L299 367.2c-8.3 10.6-21 16.8-34.4 16.8c-8.4 0-16.6-2.4-23.7-7l-25.4-16.4c-2.2-1.4-4.5-2.5-6.9-3.4zm65.2-214.8L296 164.7c10.1 10.1 2.9 27.3-11.3 27.3H254.8c-5.6 0-11.1-1.2-16.2-3.4l-42.8-19c-14.3-6.3-11.9-27.3 3.4-30.3l38.5-7.7c13.1-2.6 26.7 1.5 36.1 10.9zM248 432c0-8.8 7.2-16 16-16h16c8.8 0 16 7.2 16 16s-7.2 16-16 16H264c-8.8 0-16-7.2-16-16zM431.2 298.9l8 24c2.8 8.4-1.7 17.4-10.1 20.2s-17.4-1.7-20.2-10.1l-8-24c-2.8-8.4 1.7-17.4 10.1-20.2s17.4 1.7 20.2 10.1zm-19.9 80.4l-32 32c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l32-32c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"/></svg>;
 
   // Render error state if data couldn't be loaded
   if (!data) return <p>Could not load Country data</p>;
@@ -153,26 +154,28 @@ export default function Page() {
         )}
         </div>
         </div>
-      <div className="m-4 grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-16 rounded pt-4">   
+      <div className="m-4 grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-12 rounded pt-4">   
       {filteredData.map((country: Country) => (
         <Link
           href={`/country/${country.name.common}`}
           key={country.name.common}
-          className="block mb-4 bg-white rounded shadow hover:shadow-md dark:bg-gray-800 dark:text-white"
+          className="block mb-4 bg-white rounded shadow hover:shadow-md dark:bg-gray-800 dark:text-white hover:scale-105 duration-300"
         >
-          <div>
-            <img tabIndex={-1} className="rounded-t" src={country.flags.svg} alt={`${country.name.common} Flag`} />
-            <div className="m-6">
-            <p tabIndex={0} className="text-gray-900 text-lg font-semibold mt-2 dark:text-white">
-              {country.name.common}
-              </p>
-              <div className="my-4 mb-14 text-sm">
-                <p>Population: <span className="font-thin">{country.population}</span></p>
-                <p>Region: <span className="font-thin">{ country.region}</span></p>
-                <p>Capital: <span className="font-thin">{country.capital}</span></p>
-              </div>
-            </div>
-          </div>
+          <div className="flex flex-col h-full">
+      <img tabIndex={-1} className="rounded-t" src={country.flags.svg} alt={`${country.name.common} Flag`} />
+
+      <div className="textBox m-6 flex-grow">
+        <p tabIndex={0} className="text-gray-900 text-lg font-semibold mt-2 dark:text-white">
+          {country.name.common}
+        </p>
+        <div className="my-4 text-sm">
+          <p>Population: <span className="font-thin">{country.population}</span></p>
+          <p>Region: <span className="font-thin">{country.region}</span></p>
+          <p>Capital: <span className="font-thin">{country.capital}</span></p>
+        </div>
+      </div>
+    </div>
+
         </Link>
       ))}
       </div>
